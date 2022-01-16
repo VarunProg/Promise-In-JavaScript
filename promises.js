5dadca6eb9ee7f995b326b6804a5a6f1d22c420a
Varun Kumar <89865506+VarunProg@users.noreply.github.com>
2022-01-16 15:37:46 +0100

    resole and reject work explained

--- promises.js
+++ promises.js
@@ -1,1 +1,9 @@
 // promises
+
+//whenever promise created it takes one function which runs automatically and this function contains the result
+// promises functuon takes two paramerets which is resolve or reject
+let myPromise = new Promise(function (resolve, reject) {
+    //resolve(); this called when promise is fullfilled;
+    //reject(); this reject is called when promise is not fullfilled like some error occured
+
+})
